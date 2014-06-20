78246ba80e653e5ec047a239207546db587e005b
on-three <on_three@outlook.com>
2014-06-19 22:52:14 -0700

    Fixed issue with unicode characters.

--- markup.py
+++ markup.py
@@ -19,58 +19,58 @@ import re
 import string
 
 colors = {
-  'white': u'00',
-  'black': u'01',
-  'darkblue': u'02',
-  'green': u'03',
-  'red': u'04',
-  'brown': u'05',
-  'purple': u'06',
-  'olive': u'07',
-  'yellow': u'08',
-  'green': u'09',
-  'teal': u'10',
-  'cyan': u'11',
-  'blue': u'12',
-  'magenta': u'13',
-  'darkgray': u'14',
-  'gray': u'15',
+  u'white': u'00',
+  u'black': u'01',
+  u'darkblue': u'02',
+  u'green': u'03',
+  u'red': u'04',
+  u'brown': u'05',
+  u'purple': u'06',
+  u'olive': u'07',
+  u'yellow': u'08',
+  u'green': u'09',
+  u'teal': u'10',
+  u'cyan': u'11',
+  u'blue': u'12',
+  u'magenta': u'13',
+  u'darkgray': u'14',
+  u'gray': u'15',
 }
 
 styles = {
-  'bold' :  u'\u0002',
-  'normal' : u'\u000f',
-  'underline' : u'\u001f',
-  'italic' : u'\u0009',
-  'strikethrough' : u'\u0013',
-  'reverse' : u'\u0016',
+  u'bold' :  u'\u0002',
+  u'normal' : u'\u000f',
+  u'underline' : u'\u001f',
+  u'italic' : u'\u0009',
+  u'strikethrough' : u'\u0013',
+  u'reverse' : u'\u0016',
 }
 
 decorators = {
-  'note' : u'♫',#♬,♪,♩
-  'copyright' : u'©',
-  'trademark' : u'™',
-  'section' : u'§',
-  'yen' : u'¥',
-  'pound' : u'£',
-  'registered' : u'®',
-  'quarter' : u'¼',
-  'half' : u'½',
-  'threequarters' : u'¾',
-  'times' : u'×',
-  'divided' : u'÷',
-  'cross' : u'†',
-  'doubleexclaimation' : u'‼',
-  'starofdavid' : u'✡',
-  'heart' : u'❤',
-  'arrow' : u'➔',
-  'sun' : u'☀',
-  'umbrella' : u'☂',
-  'radioactive' : u'☢',
-  'euro' : u'€',
-  'telephone' : u'☎',
-  'flag' : u'⚑',
-  'cut' : u'✂',
+  u'note' : u'♫',#♬,♪,♩
+  u'copyright' : u'©',
+  u'trademark' : u'™',
+  u'section' : u'§',
+  u'yen' : u'¥',
+  u'pound' : u'£',
+  u'registered' : u'®',
+  u'quarter' : u'¼',
+  u'half' : u'½',
+  u'threequarters' : u'¾',
+  u'times' : u'×',
+  u'divided' : u'÷',
+  u'cross' : u'†',
+  u'doubleexclaimation' : u'‼',
+  u'starofdavid' : u'✡',
+  u'heart' : u'❤',
+  u'arrow' : u'➔',
+  u'sun' : u'☀',
+  u'umbrella' : u'☂',
+  u'radioactive' : u'☢',
+  u'euro' : u'€',
+  u'telephone' : u'☎',
+  u'flag' : u'⚑',
+  u'cut' : u'✂',
 
 }
 
@@ -79,11 +79,11 @@ def markup():
   This supports autocompletion
   '''
   for s in styles.keys():
-    yield '[{s}]'.format(s=s)
+    yield u'[{s}]'.format(s=s)
   for c in colors.keys():
-    yield '[{c}]'.format(c=c)
+    yield u'[{c}]'.format(c=c)
   for d in decorators.keys():
-    yield '[{d}]'.format(d=d)
+    yield u'[{d}]'.format(d=d)
 
 def markup2code(m):
   ml = m.lower()
@@ -105,8 +105,8 @@ def to_color(c):
     return c
 
 #have to precompile these regexes 'cause i want them case insensitive
-SINGLE_MARKUP_RE = re.compile(r'\[(?P<markup>\w+)\]', re.I)
-DOUBLE_MARKUP_RE = re.compile(r'\[(?P<foreground>\w+) (?P<background>\w+)\]', re.I)
+SINGLE_MARKUP_RE = re.compile(ur'\[(?P<markup>\w+)\]', re.I|re.UNICODE)
+DOUBLE_MARKUP_RE = re.compile(ur'\[(?P<foreground>\w+) (?P<background>\w+)\]', re.I|re.UNICODE)
 
 # Functions
 def process_markup(data, msgtype, servername, args):
@@ -116,19 +116,20 @@ def process_markup(data, msgtype, servername, args):
   or<MESSAGE TYPE> <channel> :<msg>
   So i'll split it by colon for now.
   '''
-  header,blurb = string.split(args, ':', maxsplit=1)
+  args = args.decode('utf-8')
+  header,blurb = string.split(args, u':', maxsplit=1)
 
   #first let's tag on a "greentext" modifier for lines which start with '>'
-  blurb = re.sub(r'^\>', u'[green][bold]>', blurb)
+  blurb = re.sub(ur'^\>', u'[green]>', blurb)
   
-  blurb = re.sub(r'\[(?P<markup>\w+)\]', lambda match: u'{0}'.format(markup2code(match.group(1))), blurb)
-  blurb = re.sub(r'\[(?P<foreground>\w+) (?P<background>\w+)\]', lambda match: u"\u0003{0},{1}".format(to_color(match.group(1)),to_color(match.group(2))), blurb)
+  blurb = re.sub(ur'\[(?P<markup>\w+)\]', lambda match: u'{0}'.format(markup2code(match.group(1))), blurb)
+  blurb = re.sub(ur'\[(?P<foreground>\w+) (?P<background>\w+)\]', lambda match: u"\u0003{0},{1}".format(to_color(match.group(1)),to_color(match.group(2))), blurb)
   args = header  + ':' + blurb
   return args
 
 def markup_autocomplete(data, buffer, command):
   str_input = weechat.buffer_get_string(weechat.current_buffer(), "input")
-  if command == "/input complete_next":# and str_input == '':
+  if command == u"/input complete_next":# and str_input == '':
     last_pos = str_input.rindex('[')#fails if not found.
     last = str_input[last_pos:]
     first = str_input[:last_pos]
